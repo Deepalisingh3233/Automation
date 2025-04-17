e916551b971bcd1a67027efa8d4ff091ef4ecb4f
Deepali <deepalisingh3233@gmail.com>
2025-04-17 19:10:18 +0530

    About Constructor

---
+++ CoreJava/src/test/java/coreJava/PS3.java
@@ -0,0 +1,19 @@
+package coreJava;
+
+public class PS3 {
+	
+	int a;
+	
+	public int multipleTwo() {
+		
+		a = a*2;
+		return a;
+	}
+	
+	public int multipleThree() {
+		
+		a = a*2;
+		return a;
+	}
+
+}
